0a73d737464619e5e709ff4dd70c5d1f94dc22ee
Justin V <vietzi99@gmail.com>
2022-01-18 14:51:41 +0100

    Remove Bloc entries

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -96,10 +95,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
